32e19c2e652919ee947c3e494d64f0d3c97e9e64
Ofer Nave <odigity@gmail.com>
2022-06-26 16:28:48 -0400

    style

--- g/models.py
+++ g/models.py
@@ -5,43 +5,43 @@ from django.db                          import models
 
 
 class Advisor(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    full_name = models.CharField(max_length=50)
-    avatar = models.URLField(null=True)
+    user                = models.OneToOneField(User, on_delete=models.CASCADE)
+    full_name           = models.CharField(max_length=50)
+    avatar              = models.URLField(null=True)
 
 
 class Investor(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    full_name = models.CharField(max_length=50)
-    avatar = models.URLField(null=True)
+    user                = models.OneToOneField(User, on_delete=models.CASCADE)
+    full_name           = models.CharField(max_length=50)
+    avatar              = models.URLField(null=True)
 
 
 class PlanCategory(models.Model):
-    label = models.CharField(max_length=50)
-    icon = models.CharField(max_length=100)
+    label               = models.CharField(max_length=50)
+    icon                = models.CharField(max_length=100)
 
 
 class Plan(models.Model):
-    name = models.CharField(max_length=50)
-    category = models.ForeignKey(PlanCategory, on_delete=models.CASCADE)
+    name                = models.CharField(max_length=50)
+    category            = models.ForeignKey(PlanCategory, on_delete=models.CASCADE)
 
 
 class Notification(models.Model):
-    body = models.CharField(max_length=50)
-    metadata = models.JSONField(default=dict)
+    body                = models.CharField(max_length=50)
+    metadata            = models.JSONField(default=dict)
 
-    icon1_content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE, related_name="+")
-    icon1_object_id = models.PositiveIntegerField()
-    icon1_object = GenericForeignKey('icon1_content_type', 'icon1_object_id')
+    icon1_content_type  = models.ForeignKey(ContentType, on_delete=models.CASCADE, related_name="+")
+    icon1_object_id     = models.PositiveIntegerField()
+    icon1_object        = GenericForeignKey('icon1_content_type', 'icon1_object_id')
 
-    icon2_content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE, related_name="+")
-    icon2_object_id = models.PositiveIntegerField()
-    icon2_object = GenericForeignKey('icon2_content_type', 'icon2_object_id')
+    icon2_content_type  = models.ForeignKey(ContentType, on_delete=models.CASCADE, related_name="+")
+    icon2_object_id     = models.PositiveIntegerField()
+    icon2_object        = GenericForeignKey('icon2_content_type', 'icon2_object_id')
 
 
 class NotificationBlast(models.Model):
-    audience = models.CharField(max_length=10, choices=[('INVESTORS', 'Investors'), ('ADVISORS', 'Advisors'), ('BOTH', 'Both')])
-    body = models.CharField(max_length=50)
-    icon = models.CharField(max_length=100)
+    audience            = models.CharField(max_length=10, choices=[('INVESTORS', 'Investors'), ('ADVISORS', 'Advisors'), ('BOTH', 'Both')])
+    body                = models.CharField(max_length=50)
+    icon                = models.CharField(max_length=100)
 
 
--- g/schema.py
+++ g/schema.py
@@ -9,49 +9,49 @@ from . import models
 class User(DjangoObjectType):
     class Meta:
         model = UserModel
-        fields = ("id",)
+        fields = [ 'id' ]
 
 
 class Advisor(DjangoObjectType):
     class Meta:
         model = models.Advisor
-        fields = ("id", "user", "full_name", "avatar")
+        fields = [ 'id', 'user', 'full_name', 'avatar' ]
 
 
 class Investor(DjangoObjectType):
     class Meta:
         model = models.Investor
-        fields = ("id", "user", "full_name", "avatar")
+        fields = [ 'id', 'user', 'full_name', 'avatar' ]
 
 
 class PlanCategory(DjangoObjectType):
     class Meta:
         model = models.PlanCategory
-        fields = ("id", "label", "icon")
+        fields = [ 'id', 'label', 'icon' ]
 
 
 class Plan(DjangoObjectType):
     class Meta:
         model = models.Plan
-        fields = ("id", "name", "category")
+        fields = [ 'id', 'name', 'category' ]
 
 
 class IconObject(Union):
     class Meta:
-        types = (Advisor, Investor, Plan)
+        types = [ Advisor, Investor, Plan ]
 
 
 class Notification(DjangoObjectType):
     class Meta:
         model = models.Notification
-        fields = ("id", "body")
+        fields = [ 'id', 'body' ]
 
     icon1_object = IconObject()
     icon2_object = IconObject()
 
 
 class Queries(ObjectType):
-    hello         = String(default_value="Hi!")
+    hello         = String(default_value='Hi!')
     notifications = DjangoListField(Notification)
 
 
@@ -59,6 +59,5 @@ class Mutations(ObjectType):
     pass
 
 
-#schema = Schema(query=Queries, mutation=Mutations)
 schema = Schema(query=Queries)
-#print(schema)
+#schema = Schema(query=Queries, mutation=Mutations)
